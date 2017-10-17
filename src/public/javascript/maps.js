9768488bcf4093515a7fe88f6b94b5b7a594f254
kokopelli314 <nacclonts@gmail.com>
2017-10-16 21:54:33 -0600

    refactored/isolated the map drawing functions

--- src/public/javascript/maps.js
+++ src/public/javascript/maps.js
@@ -59,14 +59,20 @@ class CallMap {
             .await((err, usa, zipData) => this.onReady(err, usa, zipData));
     }
 
+    // draw initial map
     onReady(err, usa, zipData) {
         this.zipData = zipData;
         this.usa = usa;
-        console.log('this = ' + this);
+
+        this.drawZips(zipData);
+        this.drawStates(usa);
+    }
+
+    drawZips(zipData) {
         this.svg.insert('g', '.key')
             .attr('class', 'zips')
           .selectAll('path')
-          .data(this.zipData.features)
+          .data(zipData.features)
           .enter().append('path')
             .attr('d', this.path)
             .attr('fill', (d) => {
@@ -82,7 +88,9 @@ class CallMap {
                 let numCalls = this.calls.has(zip) ? this.calls.get(zip).value : 0;
                 return `ZIP3: ${zip}\nCalls: ${numCalls}`;
             });
+    }
 
+    drawStates(usa) {
         this.svg.insert('g', '.key')
             .attr('class', 'states')
           .selectAll('path')
@@ -91,84 +99,67 @@ class CallMap {
             .attr('d', this.path);
     }
 
+    drawKey(x, color) {
+        this.g = this.svg.append('g')
+            .attr('class', 'key')
+            .attr('transform', 'translate(0,40)');
+        this.g.selectAll('rect')
+            .data(color.range().map((d) => {
+                d = color.invertExtent(d);
+                if (d[0] == null) d[0] = x.domain()[0];
+                if (d[1] == null) d[1] = x.domain()[1];
+                return d;
+            }))
+            .enter().append('rect')
+              .attr('height', 8)
+              .attr('x', (d) => x(d[0]))
+              .attr('width', (d) => {
+                  // TODO: some values negative
+                  //if (this.x(d[1]) - this.x(d[0]) < 0) debugger;
+                  return x(d[1]) - x(d[0]);
+              } )
+              .attr('fill', (d) => color(d[0]));
+
+        this.g.append('text')
+            .attr('class', 'caption')
+            .attr('x', x.range()[0])
+            .attr('y', -6)
+            .attr('fill', '#000')
+            .attr('text-anchor', 'start')
+            .attr('font-weight', 'bold')
+            .text('Calls offered');
+          this.g.call(d3.axisBottom(x)
+            .tickSize(13)
+            .tickFormat(d3.format('d'))
+            .tickValues(color.domain()))
+          .select('.domain')
+            .remove();
+    }
+
     update(callData) {
+        // format the data properly
         let processedData = this.process(callData);
+        // match calls to keys (zip codes)
         this.calls = d3.map(processedData, (d) => d.key);
+
+        // update domain and range
         let maxValue = d3.max(processedData, (d) => d.value);
         this.x.domain(d3.range(1, maxValue, maxValue-2));
         this.color.domain(d3.range(1, maxValue, maxValue / 9));
 
         // Key / legend
-        this.g.remove()
-            .exit();
-            // .data(this.calls);
+        // remove then redraw the color key
+        this.g.remove().exit();
+        this.drawKey(this.x, this.color);
 
-        this.g = this.svg.append('g')
-            .attr('class', 'key')
-            .attr('transform', 'translate(0,40)');
-        this.g.selectAll('rect')
-            .data(this.color.range().map((d) => {
-                d = this.color.invertExtent(d);
-                if (d[0] == null) d[0] = this.x.domain()[0];
-                if (d[1] == null) d[1] = this.x.domain()[1];
-                return d;
-            }))
-            .enter().append('rect')
-              .attr('height', 8)
-              .attr('x', (d) => this.x(d[0]))
-              .attr('width', (d) => {
-                  // TODO: some values negative
-                  //if (this.x(d[1]) - this.x(d[0]) < 0) debugger;
-                  return this.x(d[1]) - this.x(d[0]);
-              } )
-              .attr('fill', (d) => this.color(d[0]));
-
-        this.g.append('text')
-          .attr('class', 'caption')
-          .attr('x', this.x.range()[0])
-          .attr('y', -6)
-          .attr('fill', '#000')
-          .attr('text-anchor', 'start')
-          .attr('font-weight', 'bold')
-          .text('Calls offered');
-        this.g.call(d3.axisBottom(this.x)
-          .tickSize(13)
-          .tickFormat(d3.format('d'))
-          .tickValues(this.color.domain()))
-        .select('.domain')
-          .remove();
-
-
-        // data
+        // data - paint the state lines and zip codes
+        // clear old states and zips
         this.svg.selectAll('.zips, .states').remove().exit();
-        this.svg.insert('g', '.key')
-            .attr('class', 'zips')
-          .selectAll('path')
-          .data(this.zipData.features)
-          .enter().append('path')
-            .attr('d', this.path)
-            .attr('fill', (d) => {
-                let zip = d.properties.ZIP;
-                let val = this.calls.has(zip)
-                    ? this.calls.get(zip).value
-                    : 0;
-                return this.color(val);
-            })
-          .append('title')
-            .text((d) => {
-                let zip = d.properties.ZIP;
-                let numCalls = this.calls.has(zip) ? this.calls.get(zip).value : 0;
-                return `ZIP3: ${zip}\nCalls: ${numCalls}`;
-            });
-        
-        this.svg.insert('g', '.key')
-            .attr('class', 'states')
-          .selectAll('path')
-          .data(this.usa.features)
-          .enter().append('path')
-            .attr('d', this.path);
+        // then rebuild them
+        this.drawZips(this.zipData);
+        this.drawStates(this.usa);
 
-        console.log(processedData);
+        console.log('update done!');
     }
 
     // Takes call data (by zip code), returns total calls by ZIP3
