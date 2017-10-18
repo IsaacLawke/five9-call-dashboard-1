3a6fc3d9177009a1033f89615b0397105e76080b
kokopelli314 <nacclonts@gmail.com>
2017-10-17 20:48:23 -0600

    cleaned up duplicated code

--- src/five9-interface.js
+++ src/five9-interface.js
@@ -22,126 +22,18 @@ function jsonToSOAP(json, adminOrSupervisor='admin') {
 
 // Create a request to the Five9 Statistics API.
 // Returns promise.
-function statsRequest(message, auth) {    // Generate SOAP message for Five9
-    // Options for HTTP requests
-    const options = {
-        hostname: 'api.five9.com',
-        path: '/wssupervisor/v9_5/SupervisorWebService',
-        method: 'POST',
-        headers: {
-            'Authorization': 'Basic ' + auth,
-            'Content-Type': 'text/xml;charset=UTF-8',
-            'Content-Length': Buffer.byteLength(message),
-            'SOAPAction': ''
-        }
-    };
-
-    // Wrap in promise
-    return new Promise((resolve, reject) => {
-        var req = https.request(options, (res) => {
-            // console.log('\n---------------------------------------');
-            // console.log('---- Status:', res.statusCode, res.statusMessage);
-            // console.log('---- Headers:', res.headers);
-
-            var data = [];
-            res.on('data', (d) => {
-                data.push(d);
-            });
-            res.on('end', () => {
-                var dataString = data.join('');
-                resolve(dataString);
-            });
-        });
-
-        // Send the data
-        req.write(message);
-
-        req.on('error', (e) => {
-            console.error(e);
-            reject(e);
-        });
-    });
-}
-
-
-// Create a request to the Five9 Configuration API.
-// Returns promise.
-function configRequest(message, auth) {    // Generate SOAP message for Five9
-    // Options for HTTP requests
-    const options = {
-        hostname: 'api.five9.com',
-        path: '/wsadmin/v9_5/AdminWebService',
-        method: 'POST',
-        headers: {
-            'Authorization': 'Basic ' + auth,
-            'Content-Type': 'text/xml;charset=UTF-8',
-            'Content-Length': Buffer.byteLength(message),
-            'SOAPAction': ''
-        }
-    };
-
-    // Wrap in promise
-    return new Promise((resolve, reject) => {
-        var req = https.request(options, (res) => {
-            // console.log('\n---------------------------------------');
-            // console.log('---- Status:', res.statusCode, res.statusMessage);
-            // console.log('---- Headers:', res.headers);
-
-            var data = [];
-            res.on('data', (d) => {
-                data.push(d);
-            });
-            res.on('end', () => {
-                var dataString = data.join('');
-                resolve(dataString);
-            });
-        });
-
-        // Send the data
-        req.write(message);
-
-        req.on('error', (e) => {
-            console.error(e);
-            reject(e);
-        });
-    });
-}
-
-
-// Get a given statistic's column of data
-function getStatistic(statisticType, columnName) {
-    let columnString = '';
-    if (columnName != null) {
-        columnString =
-            `<columnNames>
-                <values>
-                    <data>${columnName}</data>
-                </values>
-            </columnNames>`;
+function sendRequest(message, auth, requestType) {
+    let path;
+    if (requestType == 'statistics') {
+        path = '/wssupervisor/v9_5/SupervisorWebService';
+    } else if (requestType == 'configuration') {
+        path = '/wsadmin/v9_5/AdminWebService';
     }
 
-    const message =
-        `<x:Envelope xmlns:x="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://service.supervisor.ws.five9.com/">
-        <x:Header/>
-            <x:Body>
-                <ser:getStatistics>
-                    <statisticType>${statisticType}</statisticType>
-                    ${columnString}
-                </ser:getStatistics>
-            </x:Body>
-        </x:Envelope>`;
-
-    return statsRequest(message);
-}
-
-
-// Create a request to the Five9 Statistics API.
-// Returns promise.
-function configRequest(message, auth) {
     // Options for HTTP requests
     const options = {
         hostname: 'api.five9.com',
-        path: '/wsadmin/v9_5/AdminWebService',
+        path: path,
         method: 'POST',
         headers: {
             'Authorization': 'Basic ' + auth,
@@ -180,5 +72,4 @@ function configRequest(message, auth) {
 
 
 module.exports.jsonToSOAP = jsonToSOAP;
-module.exports.statsRequest = statsRequest;
-module.exports.configRequest = configRequest;
+module.exports.sendRequest = sendRequest;;
