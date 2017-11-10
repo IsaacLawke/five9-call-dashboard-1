957338a4e66f6aba2efb6969f6f970ad22445ae4
kokopelli314 <nacclonts@gmail.com>
2017-11-10 12:47:16 -0700

    added timestamp to logs

--- src/helpers/log.js
+++ src/helpers/log.js
@@ -1,10 +1,12 @@
+const moment = require('moment');
+
 // Utility functions for logging and recording updates
 function message(text) {
-    console.log(text);
+    console.log(`${text} at ${moment()}`);
 }
 
 function error(text) {
-    console.error(text);
+    console.error(`${text} at ${moment()}`);
 }
 
 module.exports.message = message;
