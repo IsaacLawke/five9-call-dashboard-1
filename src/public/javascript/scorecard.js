a6ed44ba108a664ce9ff48815a4fd5bb6512679d
kokopelli <nacclonts@gmail.com>
2017-12-02 22:46:30 -0700

    added highlighting for hovered dates

--- src/public/javascript/scorecard.js
+++ src/public/javascript/scorecard.js
@@ -6,44 +6,61 @@ let x = [{"Date": "2017-11-01","AHT": "12:25","ACW": "12:35","Hold": "12:31","Ca
 let prodData = [{"Date": "2017-11-01","Productivity": "0.86"}, {"Date": "2017-11-02","Productivity": "0.86"}, {"Date": "2017-11-03","Productivity": "0.86"}, {"Date": "2017-11-04","Productivity": "0.92"}, {"Date": "2017-11-05","Productivity": "0.84"}, {"Date": "2017-11-06","Productivity": "N/A"}, {"Date": "2017-11-07","Productivity": "N/A"}, {"Date": "2017-11-08","Productivity": "0.86"}, {"Date": "2017-11-09","Productivity": "0.93"}, {"Date": "2017-11-10","Productivity": "0.93"}, {"Date": "2017-11-11","Productivity": "0.87"}, {"Date": "2017-11-12","Productivity": "0.86"}, {"Date": "2017-11-13","Productivity": "N/A"}, {"Date": "2017-11-14","Productivity": "N/A"}, {"Date": "2017-11-15","Productivity": "0.86"}, {"Date": "2017-11-16","Productivity": "0.84"}, {"Date": "2017-11-17","Productivity": "0.93"}, {"Date": "2017-11-18","Productivity": "0.92"}, {"Date": "2017-11-19","Productivity": "0.84"}, {"Date": "2017-11-20","Productivity": "N/A"}, {"Date": "2017-11-21","Productivity": "N/A"}, {"Date": "2017-11-22","Productivity": "0.86"}, {"Date": "2017-11-23","Productivity": "0.79"}, {"Date": "2017-11-24","Productivity": "0.88"}, {"Date": "2017-11-25","Productivity": "0.90"}, {"Date": "2017-11-26","Productivity": "0.80"}, {"Date": "2017-11-27","Productivity": "N/A"}, {"Date": "2017-11-28","Productivity": "N/A"}, {"Date": "2017-11-29","Productivity": "0.83"}, {"Date": "2017-11-30","Productivity": "0.83"}]
 
 
-let dtvData = [{"Date": "2017-11-01","DTV Sales": "2","Rolling Total": "2.00","Pacing": "1.36","Delta": "1"}, {"Date": "2017-11-02","DTV Sales": "0.00","Rolling Total": "2.00","Pacing": "2.73","Delta": "-1"}, {"Date": "2017-11-03","DTV Sales": "1.00","Rolling Total": "3.00","Pacing": "4.09","Delta": "-1"}, {"Date": "2017-11-04","DTV Sales": "4.00","Rolling Total": "7.00","Pacing": "5.45","Delta": "2"}, {"Date": "2017-11-05","DTV Sales": "4.00","Rolling Total": "11.00","Pacing": "6.82","Delta": "4"}, {"Date": "2017-11-06","DTV Sales": "N/A","Rolling Total": "11.00","Pacing": "N/A","Delta": "N/A"}, {"Date": "2017-11-07","DTV Sales": "N/A","Rolling Total": "11.00","Pacing": "N/A","Delta": "N/A"}, {"Date": "2017-11-08","DTV Sales": "3","Rolling Total": "14.00","Pacing": "8.18","Delta": "6"}, {"Date": "2017-11-09","DTV Sales": "3.00","Rolling Total": "17.00","Pacing": "9.55","Delta": "7"}, {"Date": "2017-11-10","DTV Sales": "0.00","Rolling Total": "17.00","Pacing": "10.91","Delta": "6"}, {"Date": "2017-11-11","DTV Sales": "4.00","Rolling Total": "21.00","Pacing": "12.27","Delta": "9"}, {"Date": "2017-11-12","DTV Sales": "0.00","Rolling Total": "21.00","Pacing": "13.64","Delta": "7"}, {"Date": "2017-11-13","DTV Sales": "N/A","Rolling Total": "21.00","Pacing": "N/A","Delta": "N/A"}, {"Date": "2017-11-14","DTV Sales": "N/A","Rolling Total": "21.00","Pacing": "N/A","Delta": "N/A"}, {"Date": "2017-11-15","DTV Sales": "2","Rolling Total": "23.00","Pacing": "15.00","Delta": "8"}, {"Date": "2017-11-16","DTV Sales": "4.00","Rolling Total": "27.00","Pacing": "16.36","Delta": "11"}, {"Date": "2017-11-17","DTV Sales": "0.00","Rolling Total": "27.00","Pacing": "17.73","Delta": "9"}, {"Date": "2017-11-18","DTV Sales": "0.00","Rolling Total": "27.00","Pacing": "19.09","Delta": "8"}, {"Date": "2017-11-19","DTV Sales": "1.00","Rolling Total": "28.00","Pacing": "20.45","Delta": "8"}, {"Date": "2017-11-20","DTV Sales": "N/A","Rolling Total": "28.00","Pacing": "N/A","Delta": "N/A"}, {"Date": "2017-11-21","DTV Sales": "N/A","Rolling Total": "28.00","Pacing": "N/A","Delta": "N/A"}, {"Date": "2017-11-22","DTV Sales": "0","Rolling Total": "28.00","Pacing": "21.82","Delta": "6"}, {"Date": "2017-11-23","DTV Sales": "3.00","Rolling Total": "31.00","Pacing": "23.18","Delta": "8"}, {"Date": "2017-11-24","DTV Sales": "0.00","Rolling Total": "31.00","Pacing": "24.55","Delta": "6"}, {"Date": "2017-11-25","DTV Sales": "4.00","Rolling Total": "35.00","Pacing": "25.91","Delta": "9"}, {"Date": "2017-11-26","DTV Sales": "2.00","Rolling Total": "37.00","Pacing": "27.27","Delta": "10"}, {"Date": "2017-11-27","DTV Sales": "N/A","Rolling Total": "37.00","Pacing": "N/A","Delta": "N/A"}, {"Date": "2017-11-28","DTV Sales": "N/A","Rolling Total": "37.00","Pacing": "N/A","Delta": "N/A"}, {"Date": "2017-11-29","DTV Sales": "2","Rolling Total": "39.00","Pacing": "28.64","Delta": "10"}, {"Date": "2017-11-30","DTV Sales": "2.00","Rolling Total": "41.00","Pacing": "30.00","Delta": "11"}]
+let dtvData = [{"Date": "2017-11-01","DTV Sales": "2","Rolling Total": "2","Pacing": "1.36","Delta": "1"}, {"Date": "2017-11-02","DTV Sales": "0","Rolling Total": "2","Pacing": "2.73","Delta": "-1"}, {"Date": "2017-11-03","DTV Sales": "1","Rolling Total": "3","Pacing": "4.09","Delta": "-1"}, {"Date": "2017-11-04","DTV Sales": "4","Rolling Total": "7","Pacing": "5.45","Delta": "2"}, {"Date": "2017-11-05","DTV Sales": "4","Rolling Total": "11","Pacing": "6.82","Delta": "4"}, {"Date": "2017-11-06","DTV Sales": "N/A","Rolling Total": "11","Pacing": "N/A","Delta": "N/A"}, {"Date": "2017-11-07","DTV Sales": "N/A","Rolling Total": "11","Pacing": "N/A","Delta": "N/A"}, {"Date": "2017-11-08","DTV Sales": "3","Rolling Total": "14","Pacing": "8.18","Delta": "6"}, {"Date": "2017-11-09","DTV Sales": "3","Rolling Total": "17","Pacing": "9.55","Delta": "7"}, {"Date": "2017-11-10","DTV Sales": "0","Rolling Total": "17","Pacing": "10.91","Delta": "6"}, {"Date": "2017-11-11","DTV Sales": "4","Rolling Total": "21","Pacing": "12.27","Delta": "9"}, {"Date": "2017-11-12","DTV Sales": "0","Rolling Total": "21","Pacing": "13.64","Delta": "7"}, {"Date": "2017-11-13","DTV Sales": "N/A","Rolling Total": "21","Pacing": "N/A","Delta": "N/A"}, {"Date": "2017-11-14","DTV Sales": "N/A","Rolling Total": "21","Pacing": "N/A","Delta": "N/A"}, {"Date": "2017-11-15","DTV Sales": "2","Rolling Total": "23","Pacing": "15","Delta": "8"}, {"Date": "2017-11-16","DTV Sales": "4","Rolling Total": "27","Pacing": "16.36","Delta": "11"}, {"Date": "2017-11-17","DTV Sales": "0","Rolling Total": "27","Pacing": "17.73","Delta": "9"}, {"Date": "2017-11-18","DTV Sales": "0","Rolling Total": "27","Pacing": "19.09","Delta": "8"}, {"Date": "2017-11-19","DTV Sales": "1","Rolling Total": "28","Pacing": "20.45","Delta": "8"}, {"Date": "2017-11-20","DTV Sales": "N/A","Rolling Total": "28","Pacing": "N/A","Delta": "N/A"}, {"Date": "2017-11-21","DTV Sales": "N/A","Rolling Total": "28","Pacing": "N/A","Delta": "N/A"}, {"Date": "2017-11-22","DTV Sales": "0","Rolling Total": "28","Pacing": "21.82","Delta": "6"}, {"Date": "2017-11-23","DTV Sales": "3","Rolling Total": "31","Pacing": "23.18","Delta": "8"}, {"Date": "2017-11-24","DTV Sales": "0","Rolling Total": "31","Pacing": "24.55","Delta": "6"}, {"Date": "2017-11-25","DTV Sales": "4","Rolling Total": "35","Pacing": "25.91","Delta": "9"}, {"Date": "2017-11-26","DTV Sales": "2","Rolling Total": "37","Pacing": "27.27","Delta": "10"}, {"Date": "2017-11-27","DTV Sales": "N/A","Rolling Total": "37","Pacing": "N/A","Delta": "N/A"}, {"Date": "2017-11-28","DTV Sales": "N/A","Rolling Total": "37","Pacing": "N/A","Delta": "N/A"}, {"Date": "2017-11-29","DTV Sales": "2","Rolling Total": "39","Pacing": "28.64","Delta": "10"}, {"Date": "2017-11-30","DTV Sales": "2","Rolling Total": "41","Pacing": "30","Delta": "11"}]
 
 
 let closeRateData = [{"Date": "2017-11-01","Close Rate": "0.39","Sales": "24","Calls": "62"}, {"Date": "2017-11-02","Close Rate": "0.37","Sales": "25","Calls": "68"}, {"Date": "2017-11-03","Close Rate": "0.40","Sales": "17","Calls": "42"}, {"Date": "2017-11-04","Close Rate": "0.40","Sales": "25","Calls": "62"}, {"Date": "2017-11-05","Close Rate": "0.37","Sales": "20","Calls": "53"}, {"Date": "2017-11-06","Close Rate": "N/A","Sales": "0","Calls": "0"}, {"Date": "2017-11-07","Close Rate": "N/A","Sales": "0","Calls": "0"}, {"Date": "2017-11-08","Close Rate": "0.44","Sales": "24","Calls": "54"}, {"Date": "2017-11-09","Close Rate": "0.58","Sales": "16","Calls": "28"}, {"Date": "2017-11-10","Close Rate": "0.44","Sales": "20","Calls": "45"}, {"Date": "2017-11-11","Close Rate": "0.57","Sales": "17","Calls": "30"}, {"Date": "2017-11-12","Close Rate": "0.41","Sales": "17","Calls": "41"}, {"Date": "2017-11-13","Close Rate": "N/A","Sales": "0","Calls": "0"}, {"Date": "2017-11-14","Close Rate": "N/A","Sales": "0","Calls": "0"}, {"Date": "2017-11-15","Close Rate": "0.56","Sales": "23","Calls": "41"}, {"Date": "2017-11-16","Close Rate": "0.35","Sales": "18","Calls": "51"}, {"Date": "2017-11-17","Close Rate": "0.41","Sales": "17","Calls": "41"}, {"Date": "2017-11-18","Close Rate": "0.58","Sales": "20","Calls": "35"}, {"Date": "2017-11-19","Close Rate": "0.59","Sales": "15","Calls": "25"}, {"Date": "2017-11-20","Close Rate": "N/A","Sales": "0","Calls": "0"}, {"Date": "2017-11-21","Close Rate": "N/A","Sales": "0","Calls": "0"}, {"Date": "2017-11-22","Close Rate": "0.58","Sales": "25","Calls": "43"}, {"Date": "2017-11-23","Close Rate": "0.44","Sales": "22","Calls": "51"}, {"Date": "2017-11-24","Close Rate": "0.50","Sales": "23","Calls": "46"}, {"Date": "2017-11-25","Close Rate": "0.51","Sales": "22","Calls": "43"}, {"Date": "2017-11-26","Close Rate": "0.36","Sales": "15","Calls": "41"}, {"Date": "2017-11-27","Close Rate": "N/A","Sales": "0","Calls": "0"}, {"Date": "2017-11-28","Close Rate": "N/A","Sales": "0","Calls": "0"}, {"Date": "2017-11-29","Close Rate": "0.38","Sales": "16","Calls": "42"}, {"Date": "2017-11-30","Close Rate": "0.47","Sales": "26","Calls": "55"}]
 
 
-const closeRateMeta = {
+const CloseRateField = {
+    format: (val) => isNaN(val)
+                    ? 'N/A'
+                    : (val * 100).toFixed(0) + '%'
+}
+
+const baseMeta = {
+    format: {
+        'Date': (val) => moment(val).format('MMM D')
+    }
+}
+const closeRateMeta = mergeDeep({
     headers: ['Date', 'Close Rate', 'Sales', 'Calls']
-};
-const dtvMeta = {
+}, baseMeta);
+const dtvMeta = mergeDeep({
     headers: ['Date', 'DTV Sales', 'Rolling Total', 'Pacing', 'Delta']
-};
-const ahtMeta = {
+}, baseMeta);
+const ahtMeta = mergeDeep({
     headers: [
         'Date', 'AHT', 'ACW', 'Hold', 'Calls'
-    ],
-    formatting: {
-
-    }
-};
-const prodMeta = {
+    ]
+}, baseMeta);
+const prodMeta = mergeDeep({
     headers: ['Date', 'Productivity']
-};
+}, baseMeta);
 
 
 dataTableRow = {
-    props: ['datum', 'keys'],
-    template: `<tr>
-        <td
-          v-for="key in keys"
-          v-bind:class="styleClass(datum[key], key)">
-            {{ formatText(datum[key], key) }}
-        </td>
+    props: ['datum', 'meta', 'isHighlighted'],
+    template: `
+        <tr v-bind:class="{ highlight: isHighlighted }">
+            <td
+              v-for="key in meta.headers"
+              v-on:mouseover="highlightDate(datum)"
+              v-on:mouseleave="unhighlightDate"
+              v-bind:class="styleClass(datum[key], key)">
+                {{ formatText(datum[key], key, meta) }}
+            </td>
         </tr>`,
     methods: {
-        formatText: function (val, key) {
-            if (key == 'Date') {
-                return moment(val).format('MMM D');
+        highlightDate: function(datum) {
+            this.$emit('hover-date', datum.Date);
+        },
+        unhighlightDate: function() {
+            this.$emit('unhover-date');
+        },
+        formatText: function (val, key, meta) {
+            if (meta.format.hasOwnProperty(key)) {
+                return meta.format[key](val);
             }
             return val;
         },
@@ -60,9 +77,27 @@ dataTableRow = {
     }
 };
 
-// Parent component for data-table-row
+singleValue = {
+    props: ['value', 'title'],
+    template: `
+        <h3>{{ title }}</h3>
+        <p class="metric">{{ value }}</p>
+    `
+};
+
+widget = {
+    field: 'CloseRate',
+
+}
+
+Vue.component('widget-box', {
+    // props: ['data', aa],
+});
+
+
+// Table with fixed headers. Accepts JSON data.
 Vue.component('data-table', {
-    props: ['data', 'meta'],
+    props: ['data', 'meta', 'highlightedDate'],
     template: `
         <div class="data-table-wrapper">
         <table class="data-table">
@@ -75,9 +110,12 @@ Vue.component('data-table', {
             <tbody>
                 <tr is="data-table-row"
                   v-for="(datum, i) in data"
+                  v-on:hover-date="highlightDate"
+                  v-on:unhover-date="unhighlightDate"
                   :key="i"
                   :datum="datum"
-                  :keys="meta.headers"
+                  :meta="meta"
+                  :isHighlighted="highlightedDate == datum.Date"
                 ></tr>
             </tbody>
         </table>
@@ -85,8 +123,18 @@ Vue.component('data-table', {
     `,
     components: {
         'data-table-row': dataTableRow
+    },
+    methods: {
+        highlightDate: function(date) {
+            this.$emit('hover-date', date);
+        },
+        unhighlightDate: function() {
+            this.$emit('unhover-date');
+        }
     }
 });
+
+Vue.component('')
 
 
 let vm = new Vue({
@@ -106,7 +154,21 @@ let vm = new Vue({
         productivityMeta: prodMeta,
         showProductivity: true,
 
-        metricDescription: 'Your AHT'
+        metricDescription: 'Your AHT',
+        highlightedDate: ''
+    },
+
+    methods: {
+        highlightDate: function (date) {
+            this.highlightedDate = date;
+        },
+        unhighlightDate: function () {
+            console.log('top level unhigh');
+            this.highlightedDate = '';
+        },
+        isHighlighted: function (date) {
+            return date == highlightedDate;
+        }
     }
 })
 
@@ -129,59 +191,36 @@ let vm = new Vue({
 //     g1.render();
 //     g1.update(0.515);
 // });
-//
-// d = `Date	AHT	ACW	Hold	Calls
-// 11/1/2017	13:25	00:35	01:31	62
-// 11/2/2017	14:00	00:50	01:18	68
-// 11/3/2017	11:27	00:37	01:27	42
-// 11/4/2017	11:31	00:48	01:40	62
-// 11/5/2017	13:01	00:34	01:31	53
-// 11/6/2017	N/A	N/A	N/A	0.00
-// 11/7/2017	N/A	N/A	N/A	0.00
-// 11/8/2017	13:30	00:23	01:16	54
-// 11/9/2017	12:10	00:38	01:19	28
-// 11/10/2017	14:29	00:20	01:38	45
-// 11/11/2017	11:51	00:25	01:16	30
-// 11/12/2017	12:00	00:30	01:19	41
-// 11/13/2017	N/A	N/A	N/A	0.00
-// 11/14/2017	N/A	N/A	N/A	0.00
-// 11/15/2017	13:46	00:40	01:29	41
-// 11/16/2017	13:47	00:37	01:29	51
-// 11/17/2017	15:05	00:27	01:25	41
-// 11/18/2017	12:09	00:25	01:35	35
-// 11/19/2017	12:54	00:35	01:22	25
-// 11/20/2017	N/A	N/A	N/A	0.00
-// 11/21/2017	N/A	N/A	N/A	0.00
-// 11/22/2017	11:29	00:35	01:46	43
-// 11/23/2017	11:35	00:34	01:40	51
-// 11/24/2017	13:35	00:40	01:45	46
-// 11/25/2017	13:00	00:49	01:36	43
-// 11/26/2017	12:23	00:35	01:41	41
-// 11/27/2017	N/A	N/A	N/A	0.00
-// 11/28/2017	N/A	N/A	N/A	0.00
-// 11/29/2017	14:18	00:27	01:27	42
-// 11/30/2017	11:30	00:34	01:42	55`;
-//
-//
-//var csv is the CSV file with headers
-function csvJSON(csv, delimiter=','){
 
-  var lines=csv.split("\n");
 
-  var result = [];
+/**
+ * Simple object check.
+ * @param item
+ * @returns {boolean}
+ */
+function isObject(item) {
+    return (item && typeof item === 'object' && !Array.isArray(item));
+}
 
-  var headers=lines[0].split(delimiter);
+/**
+ * Deep merge two objects.
+ * @param target
+ * @param ...sources
+ */
+function mergeDeep(target, ...sources) {
+    if (!sources.length) return target;
+    const source = sources.shift();
 
-  for(var i=1;i<lines.length;i++){
-	  var obj = {};
-	  var currentline=lines[i].split(delimiter);
+    if (isObject(target) && isObject(source)) {
+        for (const key in source) {
+            if (isObject(source[key])) {
+                if (!target[key]) Object.assign(target, { [key]: {} });
+                mergeDeep(target[key], source[key]);
+            } else {
+                Object.assign(target, { [key]: source[key] });
+            }
+        }
+    }
 
-	  for(var j=0;j<headers.length;j++){
-		  obj[headers[j]] = currentline[j];
-	  }
-	  result.push(obj);
-  }
-
-  //return result; //JavaScript object
-  return result; //JSON
+    return mergeDeep(target, ...sources);
 }
