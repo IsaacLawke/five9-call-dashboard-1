0f361c0d7f56822251a2818625a391102a13d670
kokopelli <nacclonts@gmail.com>
2017-12-08 10:13:42 -0700

    removing five9 updates for testing

--- src/app.js
+++ src/app.js
@@ -260,11 +260,11 @@ const server = app.listen(port, async () => {
         // Update queue stats every 15 seconds
         // Five9 stats API has a limit of 500 requests per hour
         //      (1 request every 7.2 seconds).
-        queue.scheduleUpdate(15 * 1000);
-        // Start updating call database every 2.5 minutes
-        report.scheduleUpdate(2.5 * 60 * 1000);
-        // Update user list every 12 hours
-        users.scheduleUpdate(12 * 60 * 60 * 1000);
+        // queue.scheduleUpdate(15 * 1000);
+        // // Start updating call database every 2.5 minutes
+        // report.scheduleUpdate(2.5 * 60 * 1000);
+        // // Update user list every 12 hours
+        // users.scheduleUpdate(12 * 60 * 60 * 1000);
 
     } catch (err) {
         log.error(`Error occurred on server:`, err);
